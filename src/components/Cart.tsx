44ce0865daf1970eb0543e6b184143084eb0e480
Orewa-veer <pratiharvirendra@gmail.com>
2025-08-17 14:13:47 +0530

    feat: adding ui to carts

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,9 @@
 const Cart = () => {
-  return <div>Cart 5 items $3000</div>;
+  return (
+    <div className="flex justify-center bg-green-200 py-3 text-xl mb-3">
+      <div>Total Items 5: (3000/-)</div>
+    </div>
+  );
 };
 
 export default Cart;
